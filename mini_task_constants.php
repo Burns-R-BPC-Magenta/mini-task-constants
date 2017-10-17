c44a15fb4f02be8afa29453141cfc2bc4c73e7ef
Burns-R-BPC-Magenta <32098227+Burns-R-BPC-Magenta@users.noreply.github.com>
2017-10-17 14:43:40 +0100

    Update mini_task_constants.php

--- mini_task_constants.php
+++ mini_task_constants.php
@@ -2,8 +2,12 @@
 <?php
 define ("Hello", "My name is Robbie Burns and my favourite subject is computing", true);
 echo Hello;
+?>
+<?php
 $age = 17;
 echo "my age add five years is : ", $age + "5";
+?>
+<?php
 $browser = $_SERVER['HTTP_USER_AGENT']; 
 echo "Your browser is: $browser"; 
 ?>
